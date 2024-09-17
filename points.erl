c95c9f0efd1a6ae7e12309734c997f07a6b8aa6d
Angel Albino Garcia Cerrillo <105814810+AAngel777@users.noreply.github.com>
2024-09-17 10:57:47 -0700

    Create points.erl

---
+++ points.erl
@@ -0,0 +1,43 @@
+% ████████████████████████████████████████████████████████████████
+% █ Nombre del Archivo:  points.erl                                 
+% █ Autor:              Angel Albino Garcia Cerrillo                
+% █ Correo:             l21211948@tectijuana.edu.mx                   
+% █ Fecha:              06/09/2024                                    
+% █ Curso:              Programacion logica y funcional, TECNM Campus ITT       
+% █ Unidad:             1                                                                      
+% █ Objetivo:                                                       
+% █         ╔═══════════════════════════════════════════════════════════╗
+% █         ║ Ejercicio 5 Usando un plano cartesiano                    ║
+% █         ║                                                           ║
+% █         ╚═══════════════════════════════════════════════════════════╝                     
+% █                                                               
+% █                            
+% ████████████████████████████████████████████████████████████████
+% Definición del módulo para manejar puntos en un plano cartesiano.
+-module(points).
+
+% Exportamos las funciones `new/2`, `x/1`, y `y/1` para permitir la creación de
+% puntos y la obtención de sus coordenadas.
+-export([new/2, x/1, y/1]).
+
+% Definición opaca del tipo `point/0`, el cual es un mapa que contiene dos claves: `x` y `y`.
+-opaque point() :: #{x => float(), y => float()}.
+
+% Exportamos el tipo `point/0` para que pueda ser utilizado externamente.
+-export_type([point/0]).
+
+% Especificación de la función `new/2` para crear un nuevo punto.
+% Recibe dos números en coma flotante que representan las coordenadas X e Y
+% y retorna un nuevo punto como un mapa.
+-spec new(float(), float()) -> point().
+new(X, Y) -> #{x => X, y => Y}.
+
+% Especificación de la función `x/1` para obtener la coordenada X de un punto.
+% Recibe un punto y devuelve un número en coma flotante.
+-spec x(point()) -> float().
+x(#{x := X}) -> X.
+
+% Especificación de la función `y/1` para obtener la coordenada Y de un punto.
+% Recibe un punto y devuelve un número en coma flotante.
+-spec y(point()) -> float().
+y(#{y := Y}) -> Y.
