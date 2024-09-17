dac27d8a38752e8e3a9bf2cdac5717509b4a15ea
Angel Albino Garcia Cerrillo <105814810+AAngel777@users.noreply.github.com>
2024-09-17 10:55:50 -0700

    Create hola_mundo.erl

---
+++ hola_mundo.erl
@@ -0,0 +1,20 @@
+% ████████████████████████████████████████████████████████████████
+% █ Nombre del Archivo:  hola_mundo.erl                                 
+% █ Autor:              Angel Albino Garcia Cerrillo                
+% █ Correo:             l21211948@tectijuana.edu.mx                   
+% █ Fecha:              06/09/2024                                    
+% █ Curso:              Programacion logica y funcional, TECNM Campus ITT       
+% █ Unidad:             1                                                                      
+% █ Objetivo:                                                       
+% █         ╔═══════════════════════════════════════════════════════════╗
+% █         ║ Ejercicio 1 Hola mundo                                    ║
+% █         ║                                                           ║
+% █         ╚═══════════════════════════════════════════════════════════╝                     
+% █                                                               
+% █                            
+% ████████████████████████████████████████████████████████████████
+-module(hola_mundo).
+-export([main/1]).
+
+main(_) ->
+    io:format("~s~n", ["Hello, world!"]).
