45109be8b38b0e59e5f4e6cd8cf65df8f4e087f8
Angel Albino Garcia Cerrillo <105814810+AAngel777@users.noreply.github.com>
2024-09-17 10:58:49 -0700

    Create mapas.erl

---
+++ mapas.erl
@@ -0,0 +1,38 @@
+% ████████████████████████████████████████████████████████████████
+% █ Nombre del Archivo:  mapas.erl                                 
+% █ Autor:              Angel Albino Garcia Cerrillo                
+% █ Correo:             l21211948@tectijuana.edu.mx                   
+% █ Fecha:              06/09/2024                                    
+% █ Curso:              Programacion logica y funcional, TECNM Campus ITT       
+% █ Unidad:             1                                                                      
+% █ Objetivo:                                                       
+% █         ╔═══════════════════════════════════════════════════════════╗
+% █         ║ Ejercicio 8 Utilizando mapas de nodos                     ║
+% █         ║                                                           ║
+% █         ╚═══════════════════════════════════════════════════════════╝                     
+% █                                                               
+% █                            
+% ████████████████████████████████████████████████████████████████
+
+% Definición del módulo para crear y acceder a valores en un mapa.
+-module(mapas).
+
+% Exportamos las funciones `create_map/0` y `access_values/1` para hacerlas accesibles desde otros módulos.
+-export([create_map/0, access_values/1]).
+
+% Definición de la función `create_map/0`, que crea un mapa con claves y valores de diferentes tipos.
+create_map() ->
+    #{one => 1, "two" => 2.0, <<"three">> => [i, i, i]}.
+
+% Definición de la función `access_values/1`, que accede a los valores del mapa
+% utilizando las claves y luego imprime los valores obtenidos.
+access_values(Map) ->
+    % Obtención de los valores del mapa utilizando `maps:get/2`.
+    One = maps:get(one, Map),
+    Two = maps:get("two", Map),
+    Three = maps:get(<<"three">>, Map),
+    
+    % Impresión de los valores obtenidos.
+    io:format("Value of 'one': ~p~n", [One]),
+    io:format("Value of 'two': ~p~n", [Two]),
+    io:format("Value of 'three': ~p~n", [Three]).
